02e477337ed89fbdb27ce76c987346289ab854ee
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-24 13:17:46 -0400

    Update ProxyAuto.py

--- ProxyAuto.py
+++ ProxyAuto.py
@@ -16,7 +16,7 @@ def Search_ping():
         if result == 0: # Si el puerto está activo
             print ("Puerto abierto!")
             print (f"Puerto: {port}")  
-            proxy = f'152.206.85.87:{port}'
+            proxy = f'181.225.255.17:7080:{port}'
             proxy_new = S5Crypto.encrypt(f'{proxy}')
             break
         else: # Si hay error hal escanear un puerto
